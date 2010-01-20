05d3bb27ea3da1495a13926e704063ba9e2a5861
igor <galeta.igor@gmail.com>
2010-01-20 16:32:50 +0200

    update uninstallation 2

--- uninstall.rb
+++ uninstall.rb
@@ -1,8 +1,8 @@
-directory = File.join(RAILS_ROOT, '/vendor/plugins/easy-ckeditor/')
+directory = File.join(RAILS_ROOT, '/vendor/plugins/rails-ckeditor/')
 
 require "#{directory}lib/ckeditor/utils"
 require "#{directory}lib/ckeditor/version"
 
-puts "** Uninstalling Easy CKEditor Plugin version #{Ckeditor::Version.current}...."
+puts "** Uninstalling CKEditor Plugin version #{Ckeditor::Version.current}...."
 
 Ckeditor::Utils.destroy

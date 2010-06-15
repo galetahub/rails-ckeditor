3882007017425bf69cd1c50d3dde150f65f0edb3
Igor Galeta <galeta.igor@gmail.com>
2010-06-15 12:12:12 +0300

    Add support to index fields, and reorganize options

--- lib/ckeditor/view_helper.rb
+++ lib/ckeditor/view_helper.rb
@@ -7,42 +7,39 @@ module Ckeditor
     #
     # Two forms on one page:
     # <%= form_tag "one" %>
-    #   <%= ckeditor_textarea("object", "field", :id => "demo1") %>
+    #   <%= ckeditor_textarea("object", "field", :index => "1") %>
     # <% end %>
     # ...
     # <%= form_tag "two" %>
-    #   <%= ckeditor_textarea("object", "field", :id => "demo2") %>
+    #   <%= ckeditor_textarea("object", "field", :index => "2") %>
     # <% end %>
     #
     def ckeditor_textarea(object, field, options = {})
-      options.symbolize_keys!
+      options = options.dup.symbolize_keys
       
       var = options.delete(:object) if options.key?(:object)
       var ||= @template.instance_variable_get("@#{object}")
       
       value = var.send(field.to_sym) if var
-      value ||= options[:value] || ""
+      value ||= options.delete(:value) || ""
       
-      element_id = options[:id] || ckeditor_element_id(object, field)
+      element_id = options.delete(:id) || ckeditor_element_id(object, field, options.delete(:index))
+      width  = options.delete(:width) || '100%'
+      height = options.delete(:height) || '100%'
       
       textarea_options = { :id => element_id }
       
-      textarea_options[:cols] = options[:cols].nil? ? 70 : options[:cols].to_i
-      textarea_options[:rows] = options[:rows].nil? ? 20 : options[:rows].to_i
-      textarea_options[:class] = options[:class] unless options[:class].nil?
-
-      width = options[:width].nil? ? '100%' : options[:width]
-      height = options[:height].nil? ? '100%' : options[:height]
+      textarea_options[:cols]  = (options.delete(:cols) || 70).to_i
+      textarea_options[:rows]  = (options.delete(:rows) || 20).to_i
+      textarea_options[:class] = (options.delete(:class) || 'ckeditor').to_s
+      textarea_options[:style] = "width:#{width};height:#{height}"
       
-      ckeditor_options = {}
+      ckeditor_options = {:width => width, :height => height }
+      ckeditor_options[:language] = (options.delete(:language) || I18n.locale).to_s
+      ckeditor_options[:toolbar]  = options.delete(:toolbar) if options[:toolbar]
+      ckeditor_options[:skin]     = options.delete(:skin) if options[:skin]
       
-      ckeditor_options[:language] = options[:language] || I18n.locale.to_s
-      ckeditor_options[:toolbar]  = options[:toolbar] unless options[:toolbar].nil?
-      ckeditor_options[:skin]     = options[:skin]    unless options[:skin].nil?
-      ckeditor_options[:width]    = options[:width]   unless options[:width].nil?
-      ckeditor_options[:height]   = options[:height]  unless options[:height].nil?
-      
-      ckeditor_options[:swf_params] = options[:swf_params] unless options[:swf_params].nil?
+      ckeditor_options[:swf_params] = options.delete(:swf_params) if options[:swf_params]
       
       ckeditor_options[:filebrowserBrowseUrl] = Ckeditor.file_manager_uri
       ckeditor_options[:filebrowserUploadUrl] = Ckeditor.file_manager_upload_uri
@@ -51,10 +48,8 @@ module Ckeditor
       ckeditor_options[:filebrowserImageUploadUrl] = Ckeditor.file_manager_image_upload_uri
       
       output_buffer = ActiveSupport::SafeBuffer.new
-      
-      textarea_options.update(:style => "width:#{width};height:#{height}")
         
-      output_buffer << ActionView::Base::InstanceTag.new(object, field, self, var).to_text_area_tag(textarea_options)
+      output_buffer << ActionView::Base::InstanceTag.new(object, field, self, var).to_text_area_tag(textarea_options.merge(options))
       
       output_buffer << javascript_tag("CKEDITOR.replace('#{element_id}', { 
           #{ckeditor_applay_options(ckeditor_options)}
@@ -75,8 +70,8 @@ module Ckeditor
     
     protected
       
-      def ckeditor_element_id(object, field)
-        "#{object}_#{field}_editor"
+      def ckeditor_element_id(object, field, index = nil)
+        index.blank? ? "#{object}_#{field}_editor" : "#{object}_#{index}_#{field}_editor"
       end
       
       def ckeditor_applay_options(options={})

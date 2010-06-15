6b2f646b646673518e1742a4965e87e8cede7546
Igor Galeta <galeta.igor@gmail.com>
2010-06-15 16:26:30 +0300

    Add root element in json callback

--- app/controllers/ckeditor_controller.rb
+++ app/controllers/ckeditor_controller.rb
@@ -47,7 +47,7 @@ class CkeditorController < ApplicationController
     @record.user ||= current_user if respond_to?(:current_user)
     
     if @record.valid? && @record.save
-      @text = params[:CKEditor].blank? ? @record.to_json(:only=>[:id, :type], :methods=>[:url, :content_type, :size, :filename, :format_created_at]) : %Q"<script type='text/javascript'>
+      @text = params[:CKEditor].blank? ? @record.to_json(:only=>[:id, :type], :methods=>[:url, :content_type, :size, :filename, :format_created_at], :root => "asset") : %Q"<script type='text/javascript'>
         window.parent.CKEDITOR.tools.callFunction(#{params[:CKEditorFuncNum]}, '#{escape_single_quotes(@record.url(:content))}');
       </script>"
       

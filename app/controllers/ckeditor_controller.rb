b3fe3d051a0a3b3dc43eaa75b4ae03cd3b47599b
Pavel Chipiga <pavel.chipiga@gmail.com>
2010-07-18 01:36:50 +0800

    Fix wrong url call

--- app/controllers/ckeditor_controller.rb
+++ app/controllers/ckeditor_controller.rb
@@ -48,7 +48,7 @@ class CkeditorController < ApplicationController
     
     if @record.valid? && @record.save
       @text = params[:CKEditor].blank? ? @record.to_json(:only=>[:id, :type], :methods=>[:url, :content_type, :size, :filename, :format_created_at], :root => "asset") : %Q"<script type='text/javascript'>
-        window.parent.CKEDITOR.tools.callFunction(#{params[:CKEditorFuncNum]}, '#{escape_single_quotes(@record.url(:content))}');
+        window.parent.CKEDITOR.tools.callFunction(#{params[:CKEditorFuncNum]}, '#{escape_single_quotes(@record.url_content)}');
       </script>"
       
       render :text => @text

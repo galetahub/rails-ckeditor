8e4c84d72291654f0e605554c3c7f4adab73a7ea
y8 <y8@ya.ru>
2010-09-25 04:00:23 +0400

    Fix for "uninitialized constant ActiveSupport::Dependencies" bug.

--- lib/ckeditor.rb
+++ lib/ckeditor.rb
@@ -51,25 +51,33 @@ module Ckeditor
   
   # Get the image class from the image reference object.
   def self.image_model
-    @@image_model_ref.get
+    if self.class_variables.include?(:@@image_model_ref)
+      @@image_model_ref.get 
+    else
+      self.file_manager_image_model = "Ckeditor::Picture"
+      @@image_model_ref.get
+    end
   end
   
   # Set the image model reference object to access the images.
   def self.file_manager_image_model=(class_name)
     @@image_model_ref = ActiveSupport::Dependencies.ref(class_name)
   end
-  self.file_manager_image_model = "Ckeditor::Picture"
   
   # Get the file class from the file reference object.
   def self.file_model
-    @@file_model_ref.get
+    if self.class_variables.include?(:@@file_model_ref)
+      @@file_model_ref.get 
+    else
+      self.file_manager_file_model = "Ckeditor::AttachmentFile"
+      @@file_model_ref.get
+    end
   end
   
   # Set the file model reference object to access the files.
   def self.file_manager_file_model=(class_name)
     @@file_model_ref = ActiveSupport::Dependencies.ref(class_name)
   end
-  self.file_manager_file_model = "Ckeditor::AttachmentFile"
   
   # Default way to setup Ckeditor. Run rails generate ckeditor to create
   # a fresh initializer with all configuration values.

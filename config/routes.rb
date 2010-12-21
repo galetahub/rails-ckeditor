86c0b92d8cb9b8b29145be73cb6ca2c9dab09a38
Sean Kibler <github@skiblerspot.net>
2010-12-20 19:10:49 -0500

    Add route for CkeditorController#delete.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ ActionController::Routing::Routes.draw do |map|
   map.connect 'ckeditor/images', :controller => 'ckeditor', :action => 'images'
   map.connect 'ckeditor/files',  :controller => 'ckeditor', :action => 'files'
   map.connect 'ckeditor/create/:kind', :controller => 'ckeditor', :action => 'create'
+  map.connect 'ckeditor/:id', :controller => 'ckeditor', :action => 'destroy', :conditions => {:method => :delete}
 end

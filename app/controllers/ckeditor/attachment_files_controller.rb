111fda59580cd0a5bcb3d64060673c59141cbd3a
Val <val666@gmail.com>
2011-07-05 00:45:09 +0100

    Added user partition so users only see their own files

--- app/controllers/ckeditor/attachment_files_controller.rb
+++ app/controllers/ckeditor/attachment_files_controller.rb
@@ -1,22 +1,26 @@
 class Ckeditor::AttachmentFilesController < Ckeditor::BaseController
 
   def index
-    @attachments = Ckeditor.file_model.order("id DESC")
+    if Ckeditor.file_manager_partition_users and respond_to?(:current_user)
+      @attachments = Ckeditor.file_model.where(:user_id => current_user).order("id DESC")
+    else
+      @attachments = Ckeditor.file_model.order("id DESC")
+    end
     respond_with(@attachments)
   end
-  
+
   def create
     @attachment = Ckeditor.file_model.new
 	  respond_with_asset(@attachment)
   end
-  
+
   def destroy
     @attachment.destroy
     respond_with(@attachment, :location => ckeditor_attachments_path)
   end
-  
+
   protected
-  
+
     def find_asset
       @attachment = Ckeditor.file_model.find(params[:id])
     end
--- app/controllers/ckeditor/pictures_controller.rb
+++ app/controllers/ckeditor/pictures_controller.rb
@@ -1,22 +1,26 @@
 class Ckeditor::PicturesController < Ckeditor::BaseController
 
   def index
-    @pictures = Ckeditor.image_model.order("id DESC")
-    respond_with(@pictures) 
+    if Ckeditor.file_manager_partition_users and respond_to?(:current_user)
+      @pictures = Ckeditor.image_model.where(:user_id => current_user).order("id DESC")
+    else
+      @pictures = Ckeditor.image_model.order("id DESC")
+    end
+    respond_with(@pictures)
   end
-  
+
   def create
     @picture = Ckeditor.image_model.new
 	  respond_with_asset(@picture)
   end
-  
+
   def destroy
     @picture.destroy
     respond_with(@picture, :location => ckeditor_pictures_path)
   end
-  
+
   protected
-  
+
     def find_asset
       @picture = Ckeditor.image_model.find(params[:id])
     end
